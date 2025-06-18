03e991a375a86b0efb1f34efe6fc45ff9e577228
levoxtrip <levoxtrip@gmail.com>
2025-06-18 13:04:39 +0200

    Show camera image

---
+++ sketch.js
@@ -0,0 +1,40 @@
+let capture;
+let isReadCamera = true;
+let analyzing = true;
+let frozen = false;
+
+let frozenImage;
+
+let currentColor;
+
+
+function setup(){
+    createCanvas(windowWidth,windowHeight);
+    startCamera();
+
+}
+
+function draw(){
+    if(capture){
+        image(capture,0,0,width,height);
+    }
+}
+
+function startCamera(){
+    if(capture) {
+        capture.remove();
+    }
+
+    const constraints = {
+        video: {
+            facingMode: isRearCamera ? "environment" : "user",
+            width: { ideal:windowWidth},
+            height: {ideal:windowHeight}
+        }
+    }
+
+    capture = createCapture(constraints, () => {
+        document.getElementById('status').textContent = 'Camera can be used'
+    });
+    capture.hide();
+}
